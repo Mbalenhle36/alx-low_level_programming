dba94f4ba6cf8a45bddc17f004f24a7a3553742e
mbalenhle <mbalenhlenyathi42@gmail.com>
2023-02-24 16:38:46 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
  */
 void print_most_numbers(void)
 {
-	int c = 0;
+	int i;
 
-	for (; c <= 9; c++)
+	for (i = 0; i < 10; i++)
 	{
-	if (c == 2 || c == 4)
-	{
-	continue;
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
-	else
-	{
-	_putchar(c + '0');
-	}
-	}
+
 	_putchar('\n');
 }
